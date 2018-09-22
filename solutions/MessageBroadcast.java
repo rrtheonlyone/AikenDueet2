11672ff2543111149e2a7ae32c473cf26d46284a
Muhammad Irham Rasyidi Bin Zainal <35865440+xantho09@users.noreply.github.com>
2018-09-22 12:39:34 +0800

    Add files for Broadcast

---
+++ solutions/MessageBroadcast.java
@@ -0,0 +1,125 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MessageBroadcast {
+    private static class Edge {
+        String from;
+        String to;
+
+        public String getFrom() {
+            return from;
+        }
+
+        public String getTo() {
+            return to;
+        }
+
+        public Edge(String from, String to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        public static Edge parseEdge(String data) {
+            String[] tokens =  data.split("->");
+            return new Edge(tokens[0], tokens[1]);
+        }
+
+        @Override
+        public String toString() {
+            return "Edge{" +
+                    "from='" + from + '\'' +
+                    ", to='" + to + '\'' +
+                    '}';
+        }
+    }
+
+    private HashMap<String, ArrayList<Edge>> adjList;
+    private HashMap<String, Integer> incomingDegree;
+    private HashMap<String, Boolean> isVisited;
+
+    public MessageBroadcast(String[] dataArray) {
+        adjList = new HashMap<>();
+        incomingDegree = new HashMap<>();
+
+        for (String data : dataArray) {
+            addEdge(Edge.parseEdge(data));
+        }
+
+        isVisited = new HashMap<>();
+        for (String vertex : adjList.keySet()) {
+            isVisited.put(vertex, false);
+        }
+    }
+
+    public ArrayList<String> solve() {
+        ArrayList<String> output = new ArrayList<>();
+
+        for (String vertex : adjList.keySet()) {
+            if (!isVisited.get(vertex) && incomingDegree.get(vertex) == 0) {
+                output.add(vertex);
+                bfs(vertex);
+            }
+        }
+
+        for (String vertex : adjList.keySet()) {
+            // Just in case. The dude said it's unique, but may as well.
+            if (!isVisited.get(vertex)) {
+                output.add(vertex);
+                bfs(vertex);
+            }
+        }
+
+        return output;
+    }
+
+    private void addEdge(Edge edge) {
+        String from = edge.getFrom();
+        String to = edge.getTo();
+
+        if (!adjList.containsKey(from)) {
+            adjList.put(from, new ArrayList<>());
+        }
+
+        if (!adjList.containsKey(to)) {
+            adjList.put(to, new ArrayList<>());
+        }
+
+        if (!incomingDegree.containsKey(from)) {
+            incomingDegree.put(from, 0);
+        }
+
+        if (!incomingDegree.containsKey(to)) {
+            incomingDegree.put(to, 0);
+        }
+
+        adjList.get(from).add(edge);
+        incomingDegree.put(to, incomingDegree.get(to) + 1);
+    }
+
+    private void bfs(String source) {
+        ArrayDeque<String> bfsQueue = new ArrayDeque<>();
+        isVisited.put(source, true);
+        bfsQueue.push(source);
+
+        while (!bfsQueue.isEmpty()) {
+            String current = bfsQueue.poll();
+            for (Edge edge : adjList.get(current)) {
+                String next = edge.to;
+
+                if (!isVisited.get(next)) {
+                    isVisited.put(next, true);
+                    bfsQueue.push(next);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] dataArray = new String[]{"A->B" , "A->C" , "B->D" , "E->F" };
+        MessageBroadcast messageBroadcast = new MessageBroadcast(dataArray);
+
+        System.out.println(messageBroadcast.solve());
+    }
+
+}
