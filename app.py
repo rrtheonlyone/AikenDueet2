6bf415b90b44ba3c557df49a8fbd4bfdde69058d
Rahul Rajesh <rahul.rajesh@hotmail.com>
2018-09-22 15:32:51 +0800

    Added sorting game solution

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api, reqparse
 
 from python.primesum import primeSum
 from python.imagegps import ImageEXIF
+from python.sortinggame import fifteenpuzzle
 
 app = Flask(__name__)
 api = Api(app)
@@ -27,12 +28,35 @@ class PhotoGps(Resource):
 		json_data = request.get_json(force=True)
 		return ImageEXIF(json_data)
 
+class SortingGame(Resource):
+    def get(self):
+        return "This is the sorting game problem answer"
 
+    def post(self):
+        json_data = request.get_json(force=True)
+        game_data = json_data['puzzle']
+        puzzle = []
+
+        for i in game_data:
+            for j in i:
+                puzzle.append(j)
+
+        puzzle = tuple(puzzle)
+
+        if (len(puzzle) == 9):
+            gridSize = 3
+        else:
+            gridSize = 4
+
+        res = fifteenpuzzle(gridSize, puzzle)
+        answer_json = {"result": res}
+        return jsonify(answer_json)
 
 
 
 api.add_resource(PrimeSum, '/prime-sum')
 api.add_resource(PhotoGps, '/imagesGPS')
+api.add_resource(SortingGame, '/sorting-game')
 
 if __name__ == '__main__':
     app.run(debug=True)
